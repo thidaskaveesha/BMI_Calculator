a1c88567465905cbf01af9657d9ee4d697bd07ee
Thidas Kaveesha Senavirathna <85827837+thidaskaveesha@users.noreply.github.com>
2024-02-02 13:36:36 +0530

    Update BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,15 +1,34 @@
-weight = float(input("Enter your weight in kilograms :"))
-height = float(input("Enter your height in meters :"))
+from time import sleep
 
-bmi = weight / height**2 
-if bmi < 18.5 :
-    print("Underweight")
-elif bmi >= 18.5 and bmi < 25 :
-    print("Normal")
-elif bmi >= 25 and bmi < 30 :
-    print("Overweight")
-elif bmi >= 30 :
-    print("Obesity")
+def bmi():
+    try:
+        weight = float(input("Enter your weight in kilograms: "))
+        height = float(input("Enter your height in meters: "))
+    except ValueError:
+        print("Invalid input. Please enter valid numbers for weight and height.")
+        return
 
-print("\n")
-print("dont give up")
+    bmi_value = weight / height**2
+
+    def show(bmi):
+        if bmi < 18.5:
+            print("You are Underweight")
+        elif 18.5 <= bmi < 24.9:
+            print("You are Normal")
+        elif 25 <= bmi < 29.9:
+            print("You are Overweight")
+        elif bmi >= 30:
+            print("You are Obese")
+
+    print("\n")
+    show(bmi_value)
+    print("\n")
+
+while True:
+    bmi()
+    choice = input("Do you want to close this program? (y/n): \n \n")
+    if choice.lower() == "y":
+        print("\nThank you for using this package!")
+        sleep(2)
+        break
+
